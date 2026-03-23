63a69f5c70740d0265dd074e3898a96123bb082e
Marcos Gustavo em vocação <marcosgnobre@gmail.com>
2026-03-23 10:10:10 -0300

    fiz atualizações

--- projeto_app_diego.py
+++ projeto_app_diego.py
@@ -1,33 +1,67 @@
 '''
 CRUD
 
-hambugueria
+Hamburgueria 
 
 fazer um sistema de cardapio, onde o usuário possa cadastrar, visualizar, atualizar e deletar itens do cardapio e fazer um sistema de
 cadastrar a conta do google, e fazer um sistema de login, onde o usuário possa acessar a conta do google e visualizar e fazer um sistema de
-delivery, onde o cliente rastreia o pedido,
+delivery, onde o cliente rastreia o pedido
+Marcos Gustavo
 '''
 
 print('\n=== HAMBURGUERIA MSD ===')
-print('1.cadastrar conta do google')
-print('2. selecionar itens do cardápio')
-print('3. finalizar pedido')
-print('4. pedido sendo preparado')
-print('5. seu pedido saiu para entrega')
-print('6. seu pedido saiu para entrega')
-print('7. seu pedido foi entregue')
-print('0. sair')
 
+while True:
+    print('\n1. cadastrar conta do google')
+    print('2. selecionar itens do cardápio')
+    print('3. finalizar pedido')
+    print('4. rastrear pedido')
+    print('5. adicione seu endereço')
+    print('6. seu pedido saiu para entrega')
+    print('7. seu pedido foi entregue')
+    print('8. obrigado pela preferência')
+    print('0. sair')
 
+    escolha = input('\nEscolha uma opção: ')
 
-while True:# loop principal do sistema, verdadeiro para   escolha = input('\nEscolha uma opção: ') 
-#manter o programa rodando até o usuário escolher sair
+    if escolha == '1':
+        print('cadastro de conta do google...')
+        email = input('Digite seu email: ')
+        senha = input('Digite sua senha: ')
+        print('Conta cadastrada com sucesso!')
+        
+    elif escolha == '2':
+        print('selecionar itens do cardápio')
+        print('1. hambúrguer')
+        print('2. batata frita')
+        print('3. refrigerante')
+        item = input('Digite o número do item que deseja: ')
+        print('Item selecionado com sucesso!')
 
- escolha = input('\nEscolha uma opção: ')#variavel para armazenar a escolha do usuário
+    elif escolha == '3':
+        print('Seu pedido está sendo preparado...')
 
-if escolha == '1':
-    print('cadastro de conta do google...')
-    email = input('Digite seu email: ')
-    senha = input('Digite sua senha: ')
-    print('Conta cadastrada com sucesso
+    elif escolha == '4':
+        print('Pedido finalizado com sucesso!')
+
+    elif escolha == '5':
+        print('Adicione seu endereço para entrega...')
+        endereco = input('Digite seu endereço: ')
+        print('Endereço adicionado com sucesso!')   
+
+    elif escolha == '6':
+        print('Seu pedido saiu para entrega!')
+        
+    elif escolha == '7':
+        print('Seu pedido foi entregue!')
+
+    elif escolha == '8':
+        print('Obrigado pela preferência! Volte sempre!')
     
+    elif escolha == '0':
+        print('Saindo do sistema. Até logo!')
+        break
+
+    else:
+        print("Opção inválida. Por favor, tente novamente.")
+        
